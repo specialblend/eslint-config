dbe655e75fb5113e7f863a9ab55b3c63dd6fd77c
CJ Salem <cj.salem@fishtech.group>
2018-12-08 07:32:57 -0600

    test config

---
+++ index.test.js
@@ -0,0 +1,37 @@
+describe('index.js', () => {
+    test('Exports ESLint config', () => {
+        const config = require('./index');
+        expect(config).toBeInstanceOf(Object);
+    });
+    describe('Exports correct ESLint settings', () => {
+        const config = require('./index');
+        expect(config).toHaveProperty('env');
+        const { env } = config;
+        test('Exports correct parserOptions', () => {
+            expect(config).toHaveProperty('parserOptions');
+            const { parserOptions } = config;
+            expect(parserOptions).toHaveProperty('ecmaVersion', 2018);
+            expect(parserOptions).toHaveProperty('ecmaFeatures');
+            const { ecmaFeatures } = parserOptions;
+            expect(ecmaFeatures).toHaveProperty('jsx', true);
+        });
+        describe('Exports correct env', () => {
+            test('Node support is enabled', () => {
+                expect(env).toHaveProperty('es6', true);
+            });
+            test('Node support is enabled', () => {
+                expect(env).toHaveProperty('node', true);
+            });
+            test('Jest support is enabled', () => {
+                expect(env).toHaveProperty('jest', true);
+            });
+        });
+        test('Exports correct react settings', () => {
+            expect(config).toHaveProperty('settings');
+            const { settings } = config;
+            expect(settings).toHaveProperty('react');
+            const { react } = settings;
+            expect(react).toHaveProperty('version');
+        });
+    });
+});
